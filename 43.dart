92773910d4a8eba490d01df9d917cef11cb90ed3
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-11-26 23:21:19 +0530

    Add files via upload

---
+++ 43.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+
+void main()
+{
+  
+ for(int i=1; i<=5; i++)
+ {
+  int x = i-1;
+
+  for(int j=1; j<=i; j++)
+  {
+   
+     stdout.write('${x+i} ');
+     x+=2;
+  
+  }
+  for(int k=1; k<5; k++)
+  {
+     stdout.write(' ');
+  }
+  print('');
+ }
+}
